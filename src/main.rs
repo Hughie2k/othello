b2612d5e94e9ff402300a65964e37e8cd21baf5a
Hughie2k <100051483+Hughie2k@users.noreply.github.com>
2022-09-16 22:56:31 +0100

    Made it work in powershell with windows crate

--- src/main.rs
+++ src/main.rs
@@ -81,5 +81,17 @@ pub fn play() {
 }
 
 fn main() {
+    unsafe {
+        use windows::Win32::System::Console::*;
+        let stdout = GetStdHandle(STD_OUTPUT_HANDLE).unwrap();
+        let stderr = GetStdHandle(STD_ERROR_HANDLE).unwrap();
+        let mut mode = CONSOLE_MODE(0);
+        GetConsoleMode(stdout, &mut mode);
+        mode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+        SetConsoleMode(stdout, mode);
+        GetConsoleMode(stderr, &mut mode);
+        mode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+        SetConsoleMode(stderr, mode);
+    }
     play();
 }
